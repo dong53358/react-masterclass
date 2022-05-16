40838715271cc8d8d66039e202bfb037b979c6c2
dong53358 <dong53358@naver.com>
2022-05-16 18:42:29 +0900

    animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+`;
+
+const rotateAnimation = keyframes`
+0% {
+  transform:rotate(0deg);
+  border-radius: 0px;
+}
+50%{
+  transform:rotate(360deg);
+  border-radius: 100px;
+}
+100%{
+  transform:rotate(0deg);
+  border-radius: 0px;
+}
 `;
 
 const Box = styled.div`
   height: 200px;
   width: 200px;
   background-color: tomato;
+  animation: ${rotateAnimation} 1s linear infinite;
 `;
 
 function App() {
