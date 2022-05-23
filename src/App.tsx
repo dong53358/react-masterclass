c1219f9f244374337b445c94fe7164176e454d3c
dong53358 <dong53358@naver.com>
2022-05-24 00:11:58 +0900

    typescript initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 98vh;
-  width: 98vw;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
 const Title = styled.h1`
   color: ${(props) => props.theme.textColor};
-  font-size: 40px;
-  &:hover {
-    color: teal;
-    font-size: 80px;
-  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
