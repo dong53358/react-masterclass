123dc18297ec5ab13d85617103e0cb1965e9aab5
dong53358 <dong53358@naver.com>
2022-05-25 11:55:55 +0900

    Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,21 +2,34 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 200px;
   height: 200px;
+  font-size: 25px;
+  font-weight: 600;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 3px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
